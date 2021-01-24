ea44197caf722b298199e92ff7da668d9c8a9e52
A1FR3D0-Q4 <alfredoqahx@gmail.com>
2021-01-23 18:36:47 -0600

    Subiendo cambios

--- DeskoptAutomatico/src/main/java/com/winium/DeskoptAutomatico/App.java
+++ DeskoptAutomatico/src/main/java/com/winium/DeskoptAutomatico/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println( "Modificado" );
+        
+        System.out.println("Modificamos otra vez");
     }
 }

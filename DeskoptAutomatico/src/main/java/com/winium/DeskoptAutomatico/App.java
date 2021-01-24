710231874a39dd32b8b64bdc47eed73e3d246aca
A1FR3D0-Q4 <alfredoqahx@gmail.com>
2021-01-24 11:12:50 -0600

    cambios guardados

--- DeskoptAutomatico/src/main/java/com/winium/DeskoptAutomatico/App.java
+++ DeskoptAutomatico/src/main/java/com/winium/DeskoptAutomatico/App.java
@@ -20,11 +20,11 @@ public class App
         
         System.out.println("Modificado desde local");
         
-<<<<<<< HEAD
+
         System.out.println("Modificado desde local 3");
-=======
+
         System.out.println("Modificado desde hub2");
->>>>>>> 821a21df66b1aaf4ff27ba599c459ec5dd1edd43
+
         
     }
 }
